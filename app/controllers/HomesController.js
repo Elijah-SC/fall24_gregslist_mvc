1a6d8cd9fe124fc7965174b197fe3e5c6f1f7653
Elijah SasserCollins <ElijahJSasserCollins@gmail.com>
2024-08-28 16:29:51 -0600

    drawing homes

--- app/controllers/HomesController.js
+++ app/controllers/HomesController.js
@@ -1,6 +1,18 @@
+import { AppState } from "../AppState.js"
+
 export class HomesController {
   constructor() {
     console.log(`the Homes controller is live`)
+    this.drawHomes()
+  }
+
+  drawHomes() {
+    let homeHTML = ``
+    let homes = AppState.Homes
+    homes.forEach(home => homeHTML += home.HomeCardTemplate)
+    let HomesElm = document.getElementById(`homeCards`)
+    HomesElm.innerHTML = homeHTML
+
   }
 
 }
